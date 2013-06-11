afc9b173f847a1f832809e591dc6746162fa2949
Jon Court <jon.court@orionhealth.com>
2013-06-11 14:52:44 +1200

    added simple service test against MyService

--- arquillian.karaf.blueprint.acceptance.test/src/test/java/org/js/samples/arquillian/karaf/blueprint/MyServiceTest.java
+++ arquillian.karaf.blueprint.acceptance.test/src/test/java/org/js/samples/arquillian/karaf/blueprint/MyServiceTest.java
@@ -20,10 +20,14 @@ import org.jboss.osgi.spi.OSGiManifestBuilder;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.Asset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jc.samples.arquillian.karaf.blueprint.MyService;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
 @RunWith(Arquillian.class)
 public class MyServiceTest {
@@ -49,9 +53,22 @@ public class MyServiceTest {
 
     @Inject
     public BundleContext context;
+    private ServiceReference<MyService> myServiceRef;
+    private MyService myService;
+
+    @Before
+    public void before() {
+        this.myServiceRef = this.context.getServiceReference(MyService.class);
+        this.myService = this.context.getService(this.myServiceRef);
+    }
+
+    @After
+    public void after() {
+        this.context.ungetService(this.myServiceRef);
+    }
 
     @Test
-    public void initialisationTest() {
-        Assert.assertNotNull(this.context);
+    public void testMyService() {
+        Assert.assertEquals("Echo Processed: Hello World!!", this.myService.echo("Hello World!!"));
     }
 }
